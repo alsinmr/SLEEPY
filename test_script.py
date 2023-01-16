c46fd9bcbec9bef24f0c72755337913464df5587
alsinmr <51168380+alsinmr@users.noreply.github.com>
2023-01-17 00:53:42 +0100

    Updates

--- test_script.py
+++ test_script.py
@@ -10,12 +10,13 @@ import pyRelaxSim as RS
 import numpy as np
 
 pi=np.pi
-exp=RS.Hamiltonians.ExperSys(500,['1H','13C','13C'])
+exp=RS.Hamiltonians.ExperSys(500,['1H','13C'],vr=10000)
 
 
 exp.set_inter('dipole',0,1,delta=22000)
-exp.set_inter('dipole',1,2,delta=3000,euler=[0,pi*30/180,pi*15/180])
-exp.set_inter('dipole',0,2,delta=5000)
+# exp.set_inter('dipole',1,2,delta=3000,euler=[0,pi*30/180,pi*15/180])
+# exp.set_inter('dipole',0,2,delta=5000)
+exp.set_inter('J',0,1,15)
 
 pwdavg=RS.Powder.PowderAvg(PwdType='JCP59',q=3)
 
